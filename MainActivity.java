3120a674e0956fd37aa980111e1889676d9daa5d
mahmudhasan66209-crypto <mahmudhasan66209@gmail.com>
2026-03-26 08:48:19 +0600

    MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,34 @@
+// MainActivity.java
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        
+        // আপনার HTML ফাইল লোকাল থেকে লোড করুন
+        webView.loadUrl("file:///android_asset/calculator.html");
+    }
+    
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
